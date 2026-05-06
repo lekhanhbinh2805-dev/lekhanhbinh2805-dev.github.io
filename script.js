4600e320b02c1462cd2afcaa4918f78412078c9b
lekhanhbinh2805-dev <lekhanhbinh2805@gmail.com>
2026-05-06 13:27:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Thay link này bằng URL Web App từ Google Apps Script của bạn
 const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxt8ZOfL-Eg2pKHkC7kDTyn1UlOfnQOHZLOZt5hHEqpg15WWhwTWtHd740_mf-ahJuwZw/exec';
+const mauTong = parseInt(data.mauKiemTra);
+const mauDat = parseInt(data.mauDat);
+const mauLoi = parseInt(data.mauKhongDat || 0);
+
+if (mauDat + mauLoi !== mauTong) {
+    alert("Lỗi: Tổng số mẫu Đạt và Không đạt phải bằng Số lượng mẫu kiểm tra!");
+    btn.disabled = false;
+    btn.innerText = "GỬI DỮ LIỆU";
+    return;
+}
 //------------------------------------XỬ LÝ CÁC NÚT NHẤN--------------------------------
 async function sendDataToGoogleSheet(data) {
     const response = await fetch(SCRIPT_URL, {
